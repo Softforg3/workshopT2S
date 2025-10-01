d21f67bdf0807e492f5e8ce2e664ea2b805d5fc0
Andrzej K <32167158+Softforg3@users.noreply.github.com>
2025-10-01 12:28:19 +0000

    testy storage i azure db

--- main.tf
+++ main.tf
@@ -34,3 +34,29 @@ resource "azurerm_linux_web_app" "example" {
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
 }
+
+# Azure Storage Account
+resource "azurerm_storage_account" "example" {
+  name                     = "stworkshop05storage"
+  resource_group_name      = "rg-05"
+  location                 = "westeurope"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+# Azure Database for MySQL (burstable)
+resource "azurerm_mysql_flexible_server" "burstable" {
+  name                   = "mysql-burstable-05"
+  resource_group_name    = "rg-05"
+  location               = "westeurope"
+  administrator_login    = "mysqladminuser"
+  administrator_password = "P@ssw0rd1234!"
+  sku_name               = "B_Standard_B1ms"
+  version                = "8.0.21"
+  zone                   = "1"
+  backup_retention_days  = 7
+  geo_redundant_backup_enabled = false
+  # high_availability i authentication nie są obsługiwane w tym zasobie
+}
+
